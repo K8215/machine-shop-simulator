40683f54ef3a30f39ea7c04a134138d87b057925
Kaitlyn Visconti <k8lak8@protonmail.com>
2026-04-23 20:58:48 -0400

    refactor newOperator randomization

--- src/lib/newOperator.js
+++ src/lib/newOperator.js
@@ -3,20 +3,16 @@ import operatorData from "../data/operatorSettings.json";
 
 export function newOperator() {
   const id = Math.floor(Math.random() * 9000 + 1000);
-  const firstname =
-    operatorData.firstNames[
-      Math.floor(Math.random() * operatorData.firstNames.length + 1)
-    ];
-  const lastname =
-    operatorData.lastNames[
-      Math.floor(Math.random() * operatorData.lastNames.length + 1)
-    ];
-  const specialization =
-    operatorData.specializations[
-      Math.floor(Math.random() * operatorData.specializations.length)
-    ];
-  const quirk =
-    operatorData.quirks[Math.floor(Math.random() * operatorData.quirks.length)];
+
+  const getRandom = (prop) => {
+    const selection =
+      operatorData[prop][Math.floor(Math.random() * operatorData[prop].length)];
+    return selection;
+  };
+  const firstname = getRandom("firstNames");
+  const lastname = getRandom("lastNames");
+  const specialization = getRandom("specializations");
+  const quirk = getRandom("quirks");
 
   return new Operator({
     id: `O-${id}`,
